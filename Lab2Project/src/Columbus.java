dfbf2c4c67333f5f35b8b4ebd1fc73d0f413d917
ahinojo1 <ahinojo1@mail.depaul.edu>
2018-01-18 20:37:59 -0600

    columbus

---
+++ Lab2Project/src/Columbus.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+public class Columbus extends Application{
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		Pane map = new Pane();
+		map.
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+}
+
